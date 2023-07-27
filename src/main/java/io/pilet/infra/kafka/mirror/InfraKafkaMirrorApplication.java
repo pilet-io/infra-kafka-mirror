212892a031d8863001882b825818a44ee43b1317
Alex Potapov <potapov@yahoo.com>
2023-07-27 13:27:25 -0400

    initial

---
+++ src/main/java/io/pilet/infra/kafka/mirror/InfraKafkaMirrorApplication.java
@@ -0,0 +1,13 @@
+package io.pilet.infra.kafka.mirror;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfraKafkaMirrorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InfraKafkaMirrorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/pilet/infra/kafka/mirror/InfraKafkaMirrorApplicationTests.java
@@ -0,0 +1,13 @@
+package io.pilet.infra.kafka.mirror;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfraKafkaMirrorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
